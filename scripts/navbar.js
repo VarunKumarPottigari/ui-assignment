a236652e54c3515e760323102f2d49db93023509
Varun Kumar Pottigari <varunkumarpottigari@gmail.com>
2019-04-06 14:06:30 +0530

    basic responsive nav-bar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,9 +1,10 @@
 (function () {
-    let toggleBar = document.querySelector('.nav-bar-toggle');
+    let toggleBar = document.querySelector('#toggle');
     toggleBar.addEventListener('click', toggleBarClick, false);
 
     function toggleBarClick() {
-        let navBar = document.querySelector('.nav-bar-items');
-        navBar.classList.toggle('toggle-view');
+        this.classList.toggle('on');
+        let navBar = document.querySelector('#resize');
+        navBar.classList.toggle('active');
     }
 })();
