f7a6e9fc3b179886bd7634ed49d825023290c9b5
Varun Kumar Pottigari <varunkumarpottigari@gmail.com>
2019-04-06 21:36:06 +0530

    nav-bar-adjustments, content tab

---
+++ scripts/content.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function () {
+    var myTabs = document.querySelectorAll(".content-tab");
+    function myTabClicks(tabClickEvent) {
+        for (var i = 0; i < myTabs.length; i++) {
+            myTabs[i].classList.remove("current-tab");
+        }
+        var clickedTab = tabClickEvent.currentTarget;
+        clickedTab.classList.add("current-tab");
+        tabClickEvent.preventDefault();
+        var myContentPanes = document.querySelectorAll(".tab-pane");
+        for (i = 0; i < myContentPanes.length; i++) {
+            myContentPanes[i].classList.remove("active");
+        }
+        var anchorReference = this;
+        var activePaneId = anchorReference.children[0].getAttribute("href");
+        var activePane = document.getElementById(activePaneId);
+        activePane.classList.add("active");
+    }
+    for (i = 0; i < myTabs.length; i++) {
+        myTabs[i].addEventListener("click", myTabClicks, true);
+    }
+});
